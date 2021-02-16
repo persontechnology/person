1246b570264c24dd656d7e3dff7d85e98f1cecca
persontechnology <persontechnologys@gmail.com>
2021-02-16 14:34:06 -0500

    add page messenguer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -187,7 +187,7 @@
 
 {{-- modal contactos --}}
 
-<div class="modal fade" id="contactoModal" tabindex="-1" aria-labelledby="contactoModalLabel" aria-hidden="true">
+  <div class="modal fade" id="contactoModal" tabindex="-1" aria-labelledby="contactoModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-scrollable">
       <div class="modal-content">
         <div class="modal-header">
@@ -303,4 +303,31 @@
     </div>
   </div>
   
+  {{-- messenger api --}}
+  <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v9.0'
+      });
+    };
+
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <!-- Your Chat Plugin code -->
+  <div class="fb-customerchat"
+    attribution="setup_tool"
+    page_id="707887712735256"
+    theme_color="#0084ff"
+    logged_in_greeting=" ¡Hola! como podemos ayudarte?"
+    logged_out_greeting=" ¡Hola! como podemos ayudarte?">
+  </div>
 @endsection
