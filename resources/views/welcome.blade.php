98450f34d0414218def5263d6c8b3b37e837fd65
persontechnology <persontechnologys@gmail.com>
2021-04-26 00:49:27 -0500

    config sss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -201,7 +201,7 @@
 
 
 {{-- modal contactos --}}
-@livewire('estaticas.escribenos')
+<livewire:estaticas.escribenos />
 
   {{-- modal nosotros --}}
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"aria-hidden="true">
