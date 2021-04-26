a4ff6a9b0665d03a7335b6a3140eab465652dbff
persontechnology <persontechnologys@gmail.com>
2021-04-26 18:12:34 -0500

    cambios iniotss

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/limpiar-cache', function () {
-    
+    Artisan::call('dump-autoload');
     Artisan::call('cache:clear');
     Artisan::call('config:clear');
     Artisan::call('config:cache');
+    return 'ok';
     // Artisan::call('storage:link');
     // Artisan::call('key:generate');
     // Artisan::call('migrate:fresh --seed');
