10ab2ccdefe2f47fb3a9aa877bdf4734cd2afd9f
persontechnology <persontechnologys@gmail.com>
2021-04-26 18:53:28 -0500

    4

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/limpiar-cache', function () {
-    Artisan::call('dump-autoload');
+    
     Artisan::call('cache:clear');
     Artisan::call('config:clear');
     Artisan::call('config:cache');
